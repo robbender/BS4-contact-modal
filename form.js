0ba1df2aaf07e0478a913d837f94ae3ef28c4f36
robbender <bender.rk@gmail.com>
2020-07-03 15:13:07 -0700

    Updated clear form from close button

--- form.js
+++ form.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
         console.log("failed");
         form.classList.add('was-validated');
       } else {
+        // If Valid do this...
         $.ajax({
           type: "POST",
           url: "process.php", //
@@ -28,17 +29,41 @@ $(document).ready(function() {
         console.log("passed");
         // Activatte Response Modal
         $('#form-content')
-            .modal('hide')
-            .on('hidden.bs.modal', function(e) {
-              $('#response').modal('show');
+          .modal('hide')
+          .on('hidden.bs.modal', function(e) {
+            $('#response').modal('show');
 
-              $(this).off('hidden.bs.modal') // Remove the 'on' event binding
-                .find('form')[0].reset();
-            });
+            $(this).off('hidden.bs.modal') // Remove the 'on' event binding
+              .find('form')[0].reset();
+            // $("form")[0].reset();
+          });
         console.log("sent");
+        $('#form-content').find('form')[0].reset();
+        // window.location.reload();
       }
+      // Reset on Response close button
+      // Clear Form
+      $.clearInput = function() {
+        $('form').find('input[type=text], input[type=email], input[type=textarea], textarea').val('');
+      };
+
+      $("#clear").on("click", function(e) {
+        $.clearInput();
+        $("form")[0].reset();
+        console.log("cleared")
+        e.preventDefault();
+        // window.location.reload();
+      });
+      console.log("reset");
     });
   });
+
+  // On close clear form
+  // $('#response').off('hidden.bs.modal', function() {
+  //   $.clearInput();
+  //   console.log("cleared");
+  //   window.location.reload();
+  // });
 
   // First Modal action with one modal
   // $('.modal').on('hidden.bs.modal', function() {
